005a16ef88ddb9da66715c230346c73afe900847
HeroBlackInk <drv1@outlook.com>
2024-10-20 17:23:53 +0800

    Some bugs in the line task content parser have been fixed.

--- src/taskParser.ts
+++ src/taskParser.ts
@@ -66,7 +66,8 @@ const REGEX = {
         REMOVE_CHECKBOX:  /^(-|\*)\s+\[(x|X| )\]\s/,
         REMOVE_CHECKBOX_WITH_INDENTATION: /^([ \t]*)?(-|\*)\s+\[(x|X| )\]\s/,
         REMOVE_TODOIST_LINK: /\[([^\]]+)\]\(https:\/\/todoist\.com\/showtask\?id=\d+\)/,
-        REMOVE_TODOIST_APP_LINK: /\[([^\]]+)\]\(https:\/\/app\.todoist\.com\/app\/task\/\d+\)/,
+        REMOVE_TODOIST_LINK_1: /\[([^\]]+)\]\(https:\/\/todoist\.com\/app\/task\/\d+\)/,
+        REMOVE_TODOIST_LINK_2: /\[([^\]]+)\]\(https:\/\/app\.todoist\.com\/app\/task\/\d+\)/,
     },
     ALL_TAGS: /#[\w\u4e00-\u9fa5-]+/g,
     TASK_CHECKBOX_CHECKED: /- \[(x|X)\] /,
@@ -296,7 +297,8 @@ export class TaskParser   {
     getTaskContentFromLineText(lineText:string) {
         const TaskContent = lineText.replace(REGEX.TASK_CONTENT.REMOVE_INLINE_METADATA,"")
                                     .replace(REGEX.TASK_CONTENT.REMOVE_TODOIST_LINK,"")
-                                    .replace(REGEX.TASK_CONTENT.REMOVE_TODOIST_APP_LINK,"")
+                                    .replace(REGEX.TASK_CONTENT.REMOVE_TODOIST_LINK_1,"")
+                                    .replace(REGEX.TASK_CONTENT.REMOVE_TODOIST_LINK_2,"")
                                     .replace(REGEX.TASK_CONTENT.REMOVE_PRIORITY," ") //priority 前后必须都有空格，
                                     .replace(REGEX.TASK_CONTENT.REMOVE_TAGS,"")
                                     .replace(REGEX.TASK_CONTENT.REMOVE_DATE,"")
